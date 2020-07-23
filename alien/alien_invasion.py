312badd01a1c04995c927bc2eac17b555bef17b9
lionaw <lionawlwy@163.com>
2020-07-23 16:54:06 +0800

    20200723-1

--- alien/alien_invasion.py
+++ alien/alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from alien import Alien
 import game_functions as gf
@@ -18,6 +19,8 @@ def run_game():
     ship = Ship(ai_settings,screen)
     #创建一个外星人
     alien = Alien(ai_settings,screen)
+    #创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
 
     #创建一个用于存储子弹和一个外星人的编组
     bullets = Group()
@@ -32,7 +35,7 @@ def run_game():
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
-        gf.update_aliens(ai_settings,ship,aliens)
+        gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
         gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
