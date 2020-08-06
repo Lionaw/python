9907f99afd1026f8038bddcdbf7f929d215df7a5
Lionaw <lionawlwy@163.com>
2020-08-07 02:46:10 +0800

    20200807-1

--- Django/learning_log/learning_log/learning_logs/urls.py
+++ Django/learning_log/learning_log/learning_logs/urls.py
@@ -4,7 +4,13 @@ from . import views
 
 urlpatterns = [
     #主页
-    re_path('^$',views.index,name='index')
+    re_path('^$',views.index,name='index'),
+    #显示所有的主题
+    re_path('topic/$',views.topics,name='topics'),
+    #特定主题的详细页面
+    re_path('^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
+    #用于添加新主题的页面
+    re_path('^new_topic/$',views.new_topic,name='new_topic'),
 ]
 
 app_name = 'learning_logs'
