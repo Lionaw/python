2638896d5dc6f1e9a42dcc6214aeb291d9baa8df
lionaw <lionawlwy@163.com>
2020-08-13 14:47:28 +0800

    20200813-1

--- Django/learning_log/learning_log/learning_logs/urls.py
+++ Django/learning_log/learning_log/learning_logs/urls.py
@@ -1,5 +1,6 @@
 """定义learning_logs的URL模式"""
 from django.urls import path,include,re_path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -10,7 +11,9 @@ urlpatterns = [
     #特定主题的详细页面
     re_path('^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
     #用于添加新主题的页面
-    re_path('^new_topic/$',views.new_topic,name='new_topic'),
+    url(r'^new_topic/$',views.new_topic,name='new_topic'),
+    # 用于添加新条目的页面
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 ]
 
 app_name = 'learning_logs'
